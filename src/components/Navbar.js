62f1776b6bef376b4d5577f449862be7297e204d
mundkes-tech <mundkemohil@gmail.com>
2025-05-20 11:10:40 +0530

    changes made

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,19 +3,15 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg" style={{ backgroundColor: 'lightblue' }}>
+    <nav className="navbar navbar-expand-lg" style={{ backgroundColor: 'lightblue',  padding: '16px 0'}}>
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/">Navbar</Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <Link to="/" className="navbar-brand">Home</Link>
+              <Link to="/" className="navbar-brand"  style={{fontSize: '20px' }}>Home</Link>
             </li>
             <li className="nav-item">
-              <Link to="/add-movie" className="navbar-brand">Add Movie</Link>
+              <Link to="/add-movie" className="navbar-brand"  style={{fontSize: '20px' }}>Add Movie</Link>
             </li>
           </ul>
         </div>
